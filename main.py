20ba2ee339c67e6f6a11ae80b0c0d63daae0fcab
PersianBuddy <mnajafi0014@gmail.com>
2020-04-30 20:05:56 +0430

    iniial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# TODO: get password from user
+
+# TODO: check for at least eight character in password
+# TODO: check for uppercase and lowercase in password
+# TODO: check for number in password
+# TODO: check for special characters in password
+#  special characters " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
